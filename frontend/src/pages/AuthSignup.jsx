161ae7a38fecd0449493e57bd749ff3d1cc7223f
Abenezer <abenuberhanu271@gmail.com>
2023-08-16 23:23:14 +0300

    routes add to front-end

---
+++ frontend/src/pages/AuthSignup.jsx
@@ -0,0 +1,7 @@
+
+
+export default function AuthSignup() {
+  return (
+    <div>AuthSignup</div>
+  )
+}
---
+++ frontend/src/pages/UserProducts.jsx
@@ -0,0 +1,7 @@
+
+
+export default function UserProducts() {
+  return (
+    <div>UserProducts</div>
+  )
+}
