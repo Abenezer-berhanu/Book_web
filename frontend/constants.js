f52b9689e6b62fe6d57ddb57c12a689df02a76fe
Abenezer <abenuberhanu271@gmail.com>
2023-08-19 03:53:55 +0300

    login UI start validating from backend and show error on screen and token and user data save to local storage and inside cookie

---
+++ frontend/constants.js
@@ -0,0 +1,3 @@
+const API_URI='http://localhost:3000';
+
+export {API_URI}
--- frontend/src/pages/AuthLogin.jsx
+++ frontend/src/pages/AuthLogin.jsx
@@ -1,65 +1,190 @@
 import { Card, Typography, Box, Checkbox, Button } from "@mui/material";
-import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import axios from "axios";
+import {bake_cookie} from 'sfcookies'
+import { useNavigate } from "react-router-dom";
 /////////////////////////////////////////////////////////////////////////////////////////importing from file
-import googleIcon from '../assets/googleIcon.ico.png'
+import googleIcon from "../assets/googleIcon.ico.png";
+import { API_URI } from "../../constants";
 
 export default function AuthLogin() {
-
   const [showPassword, setShowPassword] = React.useState(false);
+  const [input, setInput] = React.useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = React.useState('')
+
+  const navigate = useNavigate()
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
-return (
+
+  const handleInputChange = (e) => {
+    setInput((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSignin = async () => {
+    setError('')
+    try {
+      const res = await axios.post(`${API_URI}/user/login`, {
+        email: input.email,
+        password: input.password,
+      });
+      console.log(res)
+      if(res.status === 200){
+        const user_data = {
+          email : res.data.email,
+          name : res.data.name
+        }
+        localStorage.setItem('user_data', JSON.stringify(user_data))
+        bake_cookie('token', res.token)
+        
+        navigate('/home')
+      }
+    } catch (error) {
+      if(error){
+        setError(error.response.data.message)
+      }
+    }
+  };
+  return (
     <div>
-      <Card sx={{maxWidth: "450px",minWidth : "250px", margin: '2em auto', padding:'1em'}}>
-        <Box sx={{ display: 'flex', flexDirection:'column', gap: '.7em', marginBottom: '2em'}}>
-        <Typography sx={{color: '#101828', fontSize : '1.8rem' , fontWeight:'600', textAlign: 'center'}}>Log in</Typography>
-        <Typography variant="p" sx={{ fontSize : '.85rem', color: '#667085', textAlign: 'center'}}>Welcome back! Please enter your details.</Typography>
+      <Card
+        sx={{
+          maxWidth: "450px",
+          minWidth: "250px",
+          margin: "2em auto",
+          padding: "1em",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: ".7em",
+            marginBottom: "2em",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#101828",
+              fontSize: "1.8rem",
+              fontWeight: "600",
+              textAlign: "center",
+            }}
+          >
+            Log in
+          </Typography>
+          <Typography
+            variant="p"
+            sx={{ fontSize: ".85rem", color: "#667085", textAlign: "center" }}
+          >
+            Welcome back! Please enter your details.
+          </Typography>
         </Box>
         <form>
-        <Box sx={{ width : '80%', display: 'flex', flexDirection: 'column', margin:'auto'}}>
-        <InputLabel  sx={{color: 'black'}}>Email</InputLabel>
-          <Input placeholder="JhonDoe@gmail.com" sx={{ marginBottom: '2em'}}/>
-          <InputLabel sx={{color: 'black'}}>Password</InputLabel>
-          <Input
-            placeholder="password"
-            type={showPassword ? 'text' : 'password'}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-        </Box>
-        <Box className='login-checkbox-box' sx={{display: "flex" ,justifyContent:'space-between', fontSize: '.8rem', color: 'black', padding:'1em' , margin: '1em'}}>
-        <Box sx={{display: 'flex', alignItems: 'center'}}>
-        <Checkbox/>
-        <Typography sx={{fontSize:'.8rem'}}>Remember for 30 days</Typography>
-        </Box>
-        <Button sx={{fontSize: '.8rem',fontWeight: '700', color:'#6941C6'}}>Forgot Password</Button>
-        </Box>
-        <Button  variant="contained" sx={{ width: '100%', height:'3em'}}>Sign in</Button>
+          <Box
+            sx={{
+              width: "80%",
+              display: "flex",
+              flexDirection: "column",
+              margin: "auto",
+            }}
+          >
+            <InputLabel sx={{ color: "black" }}>Email</InputLabel>
+            <Input
+              placeholder="JhonDoe@gmail.com"
+              sx={{ marginBottom: "2em" }}
+              name="email"
+              onChange={handleInputChange}
+            />
+            <InputLabel sx={{ color: "black" }}>Password</InputLabel>
+            <Input
+              placeholder="password"
+              name="password"
+              onChange={handleInputChange}
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </Box>
+          {error && <div style={{ margin : ' 2em auto', border : '2px solid red', width: "fit-content", padding : '15px', borderRadius : '5px', textAlign: 'center', fontSize: '12px'}}>
+            <p>{error}</p>
+          </div>}
+          <Box
+            className="login-checkbox-box"
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              fontSize: ".8rem",
+              color: "black",
+              padding: "1em",
+              margin: "1em",
+            }}
+          >
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Checkbox />
+              <Typography sx={{ fontSize: ".8rem" }}>
+                Remember for 30 days
+              </Typography>
+            </Box>
+            <Button
+              sx={{ fontSize: ".8rem", fontWeight: "700", color: "#6941C6" }}
+            >
+              Forgot Password
+            </Button>
+          </Box>
+          <Button
+            variant="contained"
+            sx={{ width: "100%", height: "3em" }}
+            onClick={handleSignin}
+          >
+            Sign in
+          </Button>
         </form>
-        <Button variant="outlined" sx={{ width: '100%', height:'3em',margin: '1em auto' }}><img src={googleIcon} alt="" style={{width: '25px', margin:'0 .5em'}}/>Sign in with Google</Button>
-        <Box sx={{display:'flex', alignItems: 'center', justifyContent:'center'}}>
-          <Button sx={{ fontWeight:'bold'}}>Sign up or Register</Button>
+        <Button
+          variant="outlined"
+          sx={{ width: "100%", height: "3em", margin: "1em auto" }}
+        >
+          <img
+            src={googleIcon}
+            alt=""
+            style={{ width: "25px", margin: "0 .5em" }}
+          />
+          Sign in with Google
+        </Button>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Button sx={{ fontWeight: "bold" }}>Sign up or Register</Button>
         </Box>
       </Card>
     </div>
-  )
+  );
 }
