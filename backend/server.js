1df1ef9515c7b9ad6e04bb92d09ffa076d5499b6
Abenezer <abenuberhanu271@gmail.com>
2023-08-19 13:20:40 +0300

    user login status controlled by redux and add not found page and vaildation

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from 'cors'
 //////////////////////////////////////////////importing from my file
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js"
@@ -9,6 +10,7 @@ import productRouter from "./routes/productRoute.js"
 const app = express();
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors())
 
 dotenv.config();
 const port = 3000;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { Routes, Route } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { read_cookie } from "sfcookies";
 //////////////////////////////////////////////////////////////////// importing from components
 import Navbar from "./components/Navbar";
+import { login } from "./store/userSlice";
 //////////////////////////////////////////////////////////////////// importing from pages
 import AuthLogin from "./pages/AuthLogin";
 import AuthSignup from "./pages/AuthSignup";
@@ -13,27 +17,44 @@ import Cart from "./pages/Cart";
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 import Products from "./pages/Products";
+import Notfound from "./pages/Notfound";
 
 export default function App() {
+  const isLoggedIn = useSelector((state) => state.loggedIn.isActive);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (document.cookie) {
+      dispatch(login());
+    }
+  }, [dispatch]);
   return (
     <>
       <Routes>
         <Route element={<Navbar />}>
           <Route index element={<Home />} />
-          <Route path="/login" element={<AuthLogin />} />
-          <Route path="/signup" element={<AuthSignup />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/products/addProduct" element={<AddProduct />} />
-          <Route path="/chart" element={<Cart />} />
-          <Route path="/admin/login" element={<AdminLogin />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/products" element={<ProductsLayout />}>
-            <Route path="/products" element={<Products />} />
-            <Route
-              path="/products/myProducts/:userName"
-              element={<UserProducts />}
-            />
-          </Route>
+          {!isLoggedIn && (
+            <>
+              <Route path="/login" element={<AuthLogin />} />
+              <Route path="/signup" element={<AuthSignup />} />
+            </>
+          )}
+          {isLoggedIn && (
+            <>
+              <Route path="/products/:id" element={<ProductDetail />} />
+              <Route path="/products/addProduct" element={<AddProduct />} />
+              <Route path="/chart" element={<Cart />} />
+              <Route path="/admin/login" element={<AdminLogin />} />
+              <Route path="/admin" element={<AdminDashboard />} />
+              <Route path="/products" element={<ProductsLayout />}>
+                <Route path="/products" element={<Products />} />
+                <Route
+                  path="/products/myProducts/"
+                  element={<UserProducts />}
+                />
+              </Route>
+            </>
+          )}
+          <Route path="*" element={<Notfound />}/>
         </Route>
       </Routes>
     </>
