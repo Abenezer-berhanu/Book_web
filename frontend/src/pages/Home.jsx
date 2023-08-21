f5a45c2b69b1823a8a62491df56300c5dfbe05fb
Abenezer <abenuberhanu271@gmail.com>
2023-08-21 04:55:47 +0300

    home screen start fatching data and display loader add as well

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,14 +4,25 @@ import HomeAutoScroll from "../components/HomeAutoScroll";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
+import {useDispatch, useSelector} from 'react-redux'
+import { getProducts } from "../store/productSlice";
+import { useEffect } from "react";
+import Product from "../components/Product";
+import ReactLoading from 'react-loading'
 
 export default function Home() {
+  const {data , status} = useSelector(state => state.product)
+  const dispatch = useDispatch()
 
+  useEffect(() => {
+    dispatch(getProducts())
+  },[])
+  
   return (
     <div className="home-wrapper">
       <HomeAutoScroll />
       <Row class="d-flex justify-content-center w-100">
-        <Col md={3}>
+        <Col md={2}>
           <ListGroup>
             <ListGroup.Item>
               <h5 style={{ textAlign: "center" }}>
@@ -54,11 +65,22 @@ export default function Home() {
             </ListGroup.Item>
           </ListGroup>
         </Col>
-        <Col md={9} style={{ borderLeft: "1px solid black", padding: '10px'}}>
+        <Col md={10} style={{ borderLeft: "1px solid black", padding: '10px'}}>
           <Row><h4><strong>Products</strong></h4></Row>
-          <Row>
-            
-          </Row>
+          {status === "loading" ? <div style={{ width : 'fit-content', margin: '0 auto'}}>
+          <ReactLoading type="bars" color="#0000FF"
+                height={100} width={100} /> 
+          </div>: <Row>
+            {
+              data.map(product => {
+                return(
+                  <Col  sm={12} md={6} lg={4} xl={3} key={product._id}>
+                    <Product product={product}/>
+                  </Col>
+                )
+              })
+            }
+          </Row>  }
         </Col>
       </Row>
     </div>
